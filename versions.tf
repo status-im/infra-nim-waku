3a085101b553731d70ecf191feb42b37d29997a9
Jakub Sokołowski <jakub@status.im>
2020-09-07 22:21:07 +0200

    upgrade to terraform 0.13
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- versions.tf
+++ versions.tf
@@ -1,8 +1,22 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
   required_providers {
-    cloudflare   = " = 2.3.0"
-    digitalocean = " = 1.20.0"
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = " = 2.10.1"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = " = 1.22.2"
+    }
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = " = 1.95.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = " = 3.37.0"
+    }
   }
 }
