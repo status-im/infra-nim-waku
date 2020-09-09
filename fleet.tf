4c186cb8a23b2cfc6933592f884a4e6e9a1d7032
Jakub Sokołowski <jakub@status.im>
2020-09-09 21:10:22 +0200

    deploy a wakuv2 host
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- fleet.tf
+++ fleet.tf
@@ -17,3 +17,18 @@ module "nim_waku" {
   /* firewall */
   open_tcp_ports = [ "443" /* p2p */ ]
 }
+
+module "nim_waku_v2" {
+  source = "github.com/status-im/infra-tf-digital-ocean"
+
+  /* node type */
+  env    = "wakuv2"
+  group  = "wakuv2"
+  domain = var.domain
+
+  /* instance sizes */
+  size = "s-1vcpu-1gb"
+
+  /* firewall */
+  open_tcp_ports = [ "60000" /* p2p */ ]
+}
