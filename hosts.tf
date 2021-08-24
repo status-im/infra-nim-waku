03d590222e52877ba588dc4250a57e6e6bd71ba1
Arthur Koziel <arthur@arthurkoziel.com>
2021-08-24 17:11:05 +0800

    chat2bridge: add role and deploy on one prod host
    Signed-off-by: Arthur Koziel <arthur@arthurkoziel.com>
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts.tf
+++ hosts.tf
@@ -20,5 +20,7 @@ module "nim_hosts" {
     "80",    /* certbot */
     "443",   /* p2p websocket */
     "30303", /* p2p main */
+    /* only on node-01.gc-us-central1-a.wakuv2.prod */
+    "9000",  /* chat2bridge */
   ]
 }
