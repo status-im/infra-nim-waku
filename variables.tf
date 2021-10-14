385857cf439800068c2403e9e72b7561cfbba542
Jakub Sokołowski <jakub@status.im>
2021-10-14 14:34:04 +0200

    variables.tf: drop unused ssh_keys var
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-variable "ssh_keys" {
-  description = "Names of ssh public keys to add to created hosts"
-  type        = list(string)
-  # ssh key IDs acquired using doctl
-  default = ["16822693", "18813432", "18813461", "19525749", "20671731", "20686611"]
-}
-
 variable "domain" {
   description = "DNS Domain to update"
   type        = string
