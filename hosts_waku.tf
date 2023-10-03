b5eb0129e64d633f4d95fc6b0e208dde5d87bc0c
Anton Iakimov <yakimant@gmail.com>
2023-10-03 15:58:45 +0200

    change to new env: waku

---
+++ hosts_waku.tf
@@ -0,0 +1,35 @@
+module "hosts" {
+  source = "github.com/status-im/infra-tf-multi-provider"
+
+  /* node type */
+  group  = "waku"
+  env    = "waku"
+  stage  = terraform.workspace
+  domain = var.domain
+
+  /* scaling */
+  host_count = local.ws["hosts_count"]
+
+  /* instance sizes */
+  do_type = local.ws["do_type"] /* DigitalOcean */
+  ac_type = local.ws["ac_type"] /* Alibaba Cloud */
+  gc_type = local.ws["gc_type"] /* Google Cloud */
+
+  /* data volumes */
+  ac_data_vol_size = local.ws["data_vol_size"]
+  do_data_vol_size = local.ws["data_vol_size"]
+  gc_data_vol_size = local.ws["data_vol_size"]
+
+  /* firewall */
+  open_tcp_ports = [
+    "80",    /* certbot */
+    "443",   /* p2p websockify */
+    "8000",  /* p2p websocket */
+    "30303", /* p2p main */
+    /* only on node-01.gc-us-central1-a.wakuv2.prod */
+    "9000",  /* chat2bridge */
+  ]
+  open_udp_ports = [
+    "9000",  /* discovery v5 */
+  ]
+}
